5f41051b8850e87ef4a4690ab079b30b8cadd306
Albert Gao <albertgaohy@gmail.com>
2021-04-28 12:25:22 +1200

    add examples

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { icons } from './shared/icons';
+import React from "react";
+import styled from "styled-components";
+import { icons } from "./shared/icons";
 
-const Svg = styled.svg`
-  display: ${props => (props.block ? 'block' : 'inline-block')};
+const Svg = styled.svg<{ block: boolean }>`
+  display: ${(props) => (props.block ? "block" : "inline-block")};
   vertical-align: middle;
 
   shape-rendering: inherit;
@@ -15,6 +14,11 @@ const Path = styled.path`
   fill: currentColor;
 `;
 
+interface IconProps {
+  icon: keyof typeof icons;
+  block?: boolean;
+}
+
 /**
  * An Icon is a piece of visual element, but we must ensure its accessibility while using it.
  * It can have 2 purposes:
@@ -22,19 +26,20 @@ const Path = styled.path`
  * - *decorative only*: for example, it illustrates a label next to it. We must ensure that it is ignored by screen readers, by setting `aria-hidden` attribute (ex: `<Icon icon="check" aria-hidden />`)
  * - *non-decorative*: it means that it delivers information. For example, an icon as only child in a button. The meaning can be obvious visually, but it must have a proper text alternative via `aria-label` for screen readers. (ex: `<Icon icon="print" aria-label="Print this document" />`)
  */
-export function Icon({ icon, block, ...props }) {
+export const Icon: React.FC<IconProps> = ({
+  icon,
+  block = false,
+  ...props
+}) => {
   return (
-    <Svg viewBox="0 0 1024 1024" width="20px" height="20px" block={block} {...props}>
+    <Svg
+      viewBox="0 0 1024 1024"
+      width="20px"
+      height="20px"
+      block={block}
+      {...props}
+    >
       <Path d={icons[icon]} />
     </Svg>
   );
-}
-
-Icon.propTypes = {
-  icon: PropTypes.string.isRequired,
-  block: PropTypes.bool,
-};
-
-Icon.defaultProps = {
-  block: false,
 };
